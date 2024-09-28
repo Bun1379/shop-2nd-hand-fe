05ca8706db15d9eac3b72487c48eda4c28262897
kow111 <chome710@gmail.com>
2024-09-28 12:45:23 +0700

    handle expire token

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -3,30 +3,43 @@ const BASE_URL = `http://localhost:3000/api/v1`;
 
 // Tạo axios client chung
 const axiosClient = axios.create({
-    baseURL: BASE_URL,
-    headers: { "Content-Type": "application/json" },
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
 });
 
 const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { "Content-Type": "application/json" },
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
 });
 
 axiosPrivate.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token"); // Không cần await
-        if (!token) {
-            return Promise.reject(
-                new Error("Không có token, vui lòng đăng nhập lại.")
-            );
-        } else {
-            config.headers["Authorization"] = `Bearer ${token}`;
-            return config;
-        }
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token"); // Không cần await
+    if (!token) {
+      return Promise.reject(
+        new Error("Không có token, vui lòng đăng nhập lại.")
+      );
+    } else {
+      config.headers["Authorization"] = `Bearer ${token}`;
+      return config;
     }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosPrivate.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.data && error.response.data.EC === -999) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
 );
 
 export { axiosClient, axiosPrivate };
