b3b919e86e00bf57e86a17b021352e30548a06c6
Bun1379 <trieunam19955@gmail.com>
2024-10-02 17:04:34 +0700

    update bootstrap for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, useNavigate, useLocation } from "react-router-dom";
-import { FaBell, FaShoppingCart, FaUser, FaSearch } from "react-icons/fa";
+import { NavLink, useNavigate } from "react-router-dom";
+import { FaBell, FaShoppingCart, FaSearch } from "react-icons/fa";
 import { toast } from "react-toastify";
-import "./Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css"; // Import CSS với class .header
 
 function Header() {
-  const location = useLocation();
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user"));
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -41,24 +41,42 @@ function Header() {
   };
 
   return (
-    <nav className="header">
-      <ul className="header-nav">
-        <ul className="header-nav-left">
-          <li>
-            <NavLink to="/">Logo shop</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Nam</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Nữ</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Unisex</NavLink>
-          </li>
-        </ul>
-        <ul className="header-nav-right">
-          <li>
+    <div className="header">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid"> {/* Sử dụng container-fluid và padding */}
+          <NavLink className="navbar-brand" to="/">
+            Logo shop
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Nam
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Nữ
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Unisex
+                </NavLink>
+              </li>
+            </ul>
+
             <div className="header-search-container">
               <input
                 type="text"
@@ -66,70 +84,92 @@ function Header() {
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyDown={handleKeyPress}
+                className="form-control"
               />
               <FaSearch className="icon-search" />
             </div>
-          </li>
-          {isLoggedIn ? (
-            <>
-              <li>
-                <NavLink to="/notifications">
-                  <FaBell />
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/cart">
-                  <FaShoppingCart />
-                </NavLink>
-              </li>
-              <li
-                className="nav-item dropdown"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-              >
-                <NavLink to="#" className="nav-link">
-                  <img src={user.image} alt="Avatar" className="img-fluid rounded-circle" style={{ width: "40px", height: "40px" }} />
-                </NavLink>
 
-                {/* Dropdown Menu */}
-                {dropdownOpen && (
-                  <ul className="dropdown-menu show">
-                    <li>
-                      <NavLink className="dropdown-item" to="/user-profile">
-                        Tài khoản của tôi
-                      </NavLink>
-                    </li>
-                    <li>
-                      <NavLink
-                        className="dropdown-item"
-                        to="/user-profile"
-                        state={{ initialSection: "orders" }}
-                      >
-                        Đơn mua
-                      </NavLink>
-                    </li>
-                    <li>
-                      <button className="dropdown-item" onClick={handleLogout}>
-                        Đăng xuất
-                      </button>
-                    </li>
-                  </ul>
-                )}
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <NavLink to="/login">Đăng nhập</NavLink>
-              </li>
-              <li>
-                <NavLink to="/signup">Đăng ký</NavLink>
-              </li>
-            </>
-          )}
-        </ul>
-      </ul>
-    </nav>
+            {/* Các mục bên phải */}
+            <ul className="navbar-nav ms-auto d-flex align-items-center">
+              {isLoggedIn ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/notifications">
+                      <FaBell />
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/cart">
+                      <FaShoppingCart />
+                    </NavLink>
+                  </li>
+                  <li
+                    className="nav-item dropdown"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <NavLink
+                      className="nav-link dropdown-toggle"
+                      to="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded={dropdownOpen}
+                    >
+                      <img
+                        src={user.image}
+                        alt="Avatar"
+                        className="rounded-circle"
+                        style={{ width: "40px", height: "40px" }}
+                      />
+                    </NavLink>
+                    <ul
+                      className={`dropdown-menu dropdown-menu-end ${dropdownOpen ? "show" : ""
+                        }`}
+                    >
+                      <li>
+                        <NavLink className="dropdown-item" to="/user-profile">
+                          Tài khoản của tôi
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          className="dropdown-item"
+                          to="/user-profile"
+                          state={{ initialSection: "orders" }}
+                        >
+                          Đơn mua
+                        </NavLink>
+                      </li>
+                      <li>
+                        <button
+                          className="dropdown-item"
+                          onClick={handleLogout}
+                        >
+                          Đăng xuất
+                        </button>
+                      </li>
+                    </ul>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">
+                      Đăng nhập
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/signup">
+                      Đăng ký
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
