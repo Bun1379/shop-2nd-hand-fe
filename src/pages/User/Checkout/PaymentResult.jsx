b8c4274b89a30d2bafde18bba71e31df28eb6849
Bun1379 <trieunam19955@gmail.com>
2024-12-06 10:55:55 +0700

    VNPay

--- src/pages/User/Checkout/PaymentResult.jsx
+++ src/pages/User/Checkout/PaymentResult.jsx
@@ -1,45 +1,21 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
-import PaymentAPI from "../../../api/PaymentAPI";
 
 function PaymentResult() {
     const location = useLocation();
-    const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(null);
-    const [errorMessage, setErrorMessage] = useState("");
-
-    async function verifyPayment() {
-        try {
-            const params = new URLSearchParams(location.search);
-            const rs = await PaymentAPI.getPaymentVerify(params);
-
-            if (rs.status === 200) {
-                setIsPaymentSuccessful(true);
-                toast.success("Thanh toán thành công");
-            } else {
-                setIsPaymentSuccessful(false);
-                toast.error("Thanh toán không thành công");
-            }
-        } catch (error) {
-            setIsPaymentSuccessful(false);
-            setErrorMessage(error.response?.data?.EM || "Có lỗi xảy ra khi xác thực thanh toán");
-            toast.error("Lỗi khi thanh toán: " + error.response?.data?.EM);
-        }
-    }
-    useEffect(() => {
-        verifyPayment();
-    }, [location]);
+    const params = new URLSearchParams(location.search);
+    const OrderId = params.get("vnp_TxnRef").split('-ATTEMPT')[0];
+    const ResponseCode = params.get("vnp_ResponseCode");
 
     return (
         <div style={{ textAlign: "center", padding: "20px" }}>
             <h1>Kết quả thanh toán</h1>
-            {isPaymentSuccessful === null ? (
-                <p>Đang kiểm tra kết quả thanh toán...</p>
-            ) : isPaymentSuccessful ? (
+            {ResponseCode == "00" ? (
                 <p style={{ color: "green" }}>Thanh toán thành công! Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi.</p>
             ) : (
                 <p style={{ color: "red" }}>
-                    Thanh toán không thành công. {errorMessage && <span> Lỗi: {errorMessage}</span>}
+                    Thanh toán không thành công. Mã lỗi : {ResponseCode}. Vui lòng chụp màn hình và liên hệ với chúng tôi để xử lý.
                 </p>
             )}
             <button
