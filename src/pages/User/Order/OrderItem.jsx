81ea360d77f3bff069e36fccfcb02d05f292f28e
kow111 <chome710@gmail.com>
2025-01-17 20:29:01 +0700

    add display shipping fee

--- src/pages/User/Order/OrderItem.jsx
+++ src/pages/User/Order/OrderItem.jsx
@@ -13,7 +13,7 @@ const OrderItem = ({ order, handleOnClickOrder, handleReceive }) => {
   const handleConfirm = async () => {
     handleReceive(order._id);
     handleClose();
-  }
+  };
 
   const handleCheckOut = async () => {
     const PaymentData = {
@@ -25,11 +25,11 @@ const OrderItem = ({ order, handleOnClickOrder, handleReceive }) => {
       const response = await PaymentAPI.postPayment(PaymentData);
       if (response.status === 200) {
         const paymentUrl = response.data.DT;
-        window.open(paymentUrl, '_blank');
+        window.open(paymentUrl, "_blank");
       }
     } catch (error) {
-      console.log(error)
-      toast.error(error.response.data.message)
+      console.log(error);
+      toast.error(error.response.data.message);
     }
   };
 
@@ -46,18 +46,20 @@ const OrderItem = ({ order, handleOnClickOrder, handleReceive }) => {
       <Card className="mb-3">
         <Card.Header className="d-flex justify-content-between align-items-center bg-success text-white">
           <span className="fw-bold">Mã đơn hàng: {order._id}</span>
-          <span className="fw-bold">
-            {statusLabels[order.status]}
-          </span>
+          <span className="fw-bold">{statusLabels[order.status]}</span>
         </Card.Header>
 
         <Card.Body
           style={{ cursor: "pointer" }}
           onClick={() => handleOnClickOrder(order._id)}
         >
-          {order.status === "PENDING" && order.paymentMethod == "ONLINE" && order.paymentStatus !== "PAID" && (
-            <p className="text-danger text-center">Đơn hàng chưa thanh toán, vui lòng thanh toán đơn hàng.</p>
-          )}
+          {order.status === "PENDING" &&
+            order.paymentMethod == "ONLINE" &&
+            order.paymentStatus !== "PAID" && (
+              <p className="text-danger text-center">
+                Đơn hàng chưa thanh toán, vui lòng thanh toán đơn hàng.
+              </p>
+            )}
           {order.products.map((product) => (
             <OrderProductItem
               key={product._id}
@@ -69,9 +71,24 @@ const OrderItem = ({ order, handleOnClickOrder, handleReceive }) => {
         </Card.Body>
 
         <Card.Footer className="d-flex justify-content-end align-items-center bg-light">
-          <span className="fw-bold me-3">
-            Tổng tiền: {order.totalAmount.toLocaleString("vi-VN")} đ
-          </span>
+          <div className="me-3 d-flex flex-column fw-bold">
+            <span className="text-danger">
+              {order.shippingFee > 0
+                ? `Phí vận chuyển: ${order.shippingFee.toLocaleString(
+                    "vi-VN"
+                  )} đ`
+                : "Miễn phí vận chuyển"}
+            </span>
+            <span>
+              Tổng tiền hàng: {order.totalAmount.toLocaleString("vi-VN")} đ
+            </span>
+            Tổng cộng:{" "}
+            {order.shippingFee > 0
+              ? (order.totalAmount + order.shippingFee).toLocaleString("vi-VN")
+              : order.totalAmount.toLocaleString("vi-VN")}{" "}
+            đ
+          </div>
+          <span className="fw-bold me-3 d-flex flex-row"></span>
 
           {order.status === "SHIPPED" && (
             <button className="btn btn-warning" onClick={handleOpen}>
@@ -79,12 +96,13 @@ const OrderItem = ({ order, handleOnClickOrder, handleReceive }) => {
             </button>
           )}
 
-          {order.status === "PENDING" && order.paymentMethod == "ONLINE" && order.paymentStatus !== "PAID" && (
-            <button className="btn btn-warning" onClick={handleCheckOut}>
-              Thanh toán đơn hàng
-            </button>
-          )}
-
+          {order.status === "PENDING" &&
+            order.paymentMethod == "ONLINE" &&
+            order.paymentStatus !== "PAID" && (
+              <button className="btn btn-warning" onClick={handleCheckOut}>
+                Thanh toán đơn hàng
+              </button>
+            )}
         </Card.Footer>
       </Card>
 
@@ -93,9 +111,7 @@ const OrderItem = ({ order, handleOnClickOrder, handleReceive }) => {
         <Modal.Header closeButton>
           <Modal.Title>Xác nhận</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          Bạn có chắc chắn rằng bạn đã nhận được hàng?
-        </Modal.Body>
+        <Modal.Body>Bạn có chắc chắn rằng bạn đã nhận được hàng?</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Hủy
