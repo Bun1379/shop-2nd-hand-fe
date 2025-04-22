41ef301053ab261ef3d1b54df55081c4ef71fe67
kow111 <chome710@gmail.com>
2025-04-22 13:40:22 +0700

    UI, filter blog

--- src/pages/User/Blog/BlogPage.jsx
+++ src/pages/User/Blog/BlogPage.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import BlogAPI from "../../../api/BlogAPI";
 import BlogItem from "./BlogItem";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Form, FormControl, Row } from "react-bootstrap";
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  const [filteredBlogs, setFilteredBlogs] = useState([]);
+
   const fetchDataBlog = async () => {
     try {
       const res = await BlogAPI.GetBlog();
@@ -16,18 +19,43 @@ const BlogPage = () => {
     }
   };
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    const filteredBlogs = blogs.filter((blog) =>
+      blog.title.toLowerCase().includes(keyword.toLowerCase())
+    );
+    setFilteredBlogs(filteredBlogs);
+  };
+
   useEffect(() => {
     fetchDataBlog();
   }, []);
+
+  useEffect(() => {
+    setFilteredBlogs(blogs);
+  }, [blogs]);
 
   return (
     <div>
       <h1 className="w-100 text-center text-uppercase mb-4 mt-5 bg-white border border-2 border-success rounded p-3">
         Blog thời trang
       </h1>
+      {/* Search Form */}
+      <Form onSubmit={handleSearchSubmit} className="mb-4 d-flex gap-2">
+        <FormControl
+          type="text"
+          placeholder="Tìm kiếm..."
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          style={{ width: "300px" }}
+        />
+        <Button type="submit" variant="outline-success">
+          Tìm kiếm
+        </Button>
+      </Form>
       <Row>
-        {blogs.map((blog) => (
-          <Col key={blog._id} md={4} xs={12} className="mb-4">
+        {filteredBlogs.map((blog) => (
+          <Col key={blog._id} xs={12} sm={6} md={4} className="mb-4">
             <BlogItem key={blog._id} blog={blog} />
           </Col>
         ))}
